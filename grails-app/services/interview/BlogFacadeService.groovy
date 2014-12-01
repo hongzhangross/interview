f688d189260b62fc9d91f41d2827218175911ed3
hongzhangross <271726348@qq.com>
2014-12-01 10:06:51 +0800

    第三次提交
    第三次提交

--- grails-app/services/interview/BlogFacadeService.groovy
+++ grails-app/services/interview/BlogFacadeService.groovy
@@ -3,9 +3,10 @@ package interview
 import com.lerss.ent.api.HelloWorldFacade
 import grails.transaction.Transactional
 import com.lerss.ent.api.*
-import com.lerss.ent.api.BlogEntryDTO //此处应该在下面空一行
+import com.lerss.ent.api.BlogEntryDTO 
+
 @Transactional
-class BlogFacadeService implements BlogFacade{//大括号前面应该有空格
+class BlogFacadeService implements BlogFacade {
 	 /**
      * 获取最新的n条日志
      *
@@ -13,12 +14,11 @@ class BlogFacadeService implements BlogFacade{//大括号前面应该有空格
      * @return 日志列表
      */
     List<BlogEntryDTO> getRecentEntries(int n){
-    	def blogentrys = BlogEntry.list(offset:0, max:n)//拼写错误，题目中已经包含了entry的复数形式entries，为什么没有参考呢？
-    	//def blogsentrydto = blogentrys 没有用的代码应当及时删除掉
+    	def blogEntries = BlogEntry.list(offset:0, max:n)
     	def list = new ArrayList<BlogEntryDTO>();
         
-        blogentrys.each{ //这个地方用groovy的collect方法重写
-		    def b = new BlogEntryDTO(title:it.getTitle(),content:it.getContent(),dateCreated:it.getDateCreated())//使用groovy的语法代替getXXX的Java写法
+        blogEntries.collect{ 
+		    def b = new BlogEntryDTO(title:it.title,content:it.content,dateCreated:it.dateCreated)
 		    list.add(b)
 		}
 		
@@ -31,10 +31,15 @@ class BlogFacadeService implements BlogFacade{//大括号前面应该有空格
      * @param entry 日志
      * @exception Exception 发布失败时候会抛出异常
      */
-    void publish(BlogEntryDTO entry){
-    //题目中要求发布失败抛出异常，实际上会抛出异常吗？	
-    	def blog = new BlogEntry(title:entry.getTitle(),content:entry.getContent())
+    void publish(BlogEntryDTO entry){	
+    	def blog = new BlogEntry(title:entry.title,content:entry.content)
 		blog.setDateCreated(entry.getDateCreated())
-    	blog.save()
+    	if(!blog.save()){
+		   blog.errors.allErrors.each{
+		        logger.info(it)
+		    }
+		}else{
+		   flush:true
+		}
     }
 }
